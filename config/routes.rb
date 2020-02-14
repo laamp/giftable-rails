364fe828a6552cb1ff93e4205e577479386457f5
laamp <lance.matthew.smith@gmail.com>
2020-02-14 15:24:53 -0800

    Rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: %i[create destroy]
+
+    resources :users, only: %i[show create update] do
+      resources :lists, only: %i[index show create update destroy] do
+        resources :gifts, only: %i[create update destroy]
+      end
+    end
+
+    resources :users_to_lists, only: %i[create]
+  end
 end
